f95fce18eb2903ad9b7d52ab0a35e958f4f16e3e
Kyeongrok <oceanfog1@gmail.com>
2022-03-28 17:04:49 +0900

    add producer

--- src/main/java/com/kafka/kafkapubsubjava/controllers/KafkaController.java
+++ src/main/java/com/kafka/kafkapubsubjava/controllers/KafkaController.java
@@ -1,11 +1,19 @@
 package com.kafka.kafkapubsubjava.controllers;
 
+import com.kafka.kafkapubsubjava.services.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KafkaController {
 
+    @Autowired
+    private KafkaProducer kafkaSampleProducerService;
 
-
+    @PostMapping(value = "/sendMessage")
+    public void sendMessage(String message) {
+        kafkaSampleProducerService.sendMessage(message);
+    }
 
 }
---
+++ src/main/java/com/kafka/kafkapubsubjava/services/KafkaProducer.java
@@ -0,0 +1,17 @@
+package com.kafka.kafkapubsubjava.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        System.out.println("send message : " + message);
+        this.kafkaTemplate.send("oingdaddy", message);
+    }
+
+}
