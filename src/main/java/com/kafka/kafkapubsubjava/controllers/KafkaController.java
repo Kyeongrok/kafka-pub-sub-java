ce1fb10475bd93de65705dc0e8c2edd964a72f9c
Kyeongrok <oceanfog1@gmail.com>
2022-03-28 17:06:45 +0900

    add hello controller

--- src/main/java/com/kafka/kafkapubsubjava/controllers/KafkaController.java
+++ src/main/java/com/kafka/kafkapubsubjava/controllers/KafkaController.java
@@ -2,14 +2,23 @@ package com.kafka.kafkapubsubjava.controllers;
 
 import com.kafka.kafkapubsubjava.services.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class KafkaController {
 
     @Autowired
     private KafkaProducer kafkaSampleProducerService;
+
+    @GetMapping(value = "/hello/{id}")
+    public String hello(@PathVariable("id") String id) {
+        return id;
+    }
 
     @PostMapping(value = "/sendMessage")
     public void sendMessage(String message) {
